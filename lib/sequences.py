e824fd1f6f7a5115b5d4821f2fac05591a4ee56f
ubah feisal <feisalubah1@gmail.com>
2023-09-11 15:21:30 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def fib_recursive(n):
+    if n<= 0:
+        return []
+    elif n==1:
+        return [0]
+    elif n==2:
+        return [0,1]
+    else:
+        fib_sequence = fib_recursive (n-1)
+        fib_sequence.append(fib_sequence[-1]+ fib_sequence[-2])
+        return fib_sequence
+    
+def print_fibonacci (length):
+    fib_sequence = fib_recursive(length)
+    print(fib_sequence)
+print_fibonacci(9)
